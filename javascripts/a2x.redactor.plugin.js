99c87ec07ccdccc4d21382a5b0f195a6320790f4
Alexander Jeliuc <jeliucalexandr@gmail.com>
2014-04-09 05:29:49 +0300

    Updating image adding to quote

--- javascripts/a2x.redactor.plugin.js
+++ javascripts/a2x.redactor.plugin.js
@@ -9,21 +9,24 @@ RedactorPlugins.a2x = {
       var callback = $.proxy(function() {
         $('#redactor_modal #a2x_insert_quote').click($.proxy(function()
           {
+            var img = $('#a2ximg').val();
             var quote = $('#a2xquote').val();
             var author = $('#a2xauthor').val();
             var link = $('#a2xlink').val();
             var html = '';
             if (quote.length > 0) {
-              if(author.length > 0) {
+              if(author.length > 0 && link.length == 0) {
                 html = this.quote_src;
+                if (img.length > 0) {
+                  html = html.replace('%image%', '<footer class="top"><img src="'+img+'"/></footer>');
+                } else {
+                  html = html.replace('%image%', '');
+                }
                 html = html.replace('%quote%', quote);
                 html = html.replace('%author%', author);
 
-                if (link.length > 0) {
-                  html = html.replace('%link%', link);
-                } else {
-                  html = html.replace('%link%', '#');
-                }
+              } else if (author.length > 0 && link.length > 0) {
+                html = html.replace('%author%', '<a href="'+ link +'" target="_blank">'+author+'</a>');
               } else {
                 html = this.quote_src_without_footer;
                 html = html.replace('%quote%', quote);
@@ -47,6 +50,8 @@ RedactorPlugins.a2x = {
     modal_quote: String() +
       '<div id="redactor_modal_content">' +
       '' +
+        '<label>Image</label>' +
+        '<input type="text" id="a2ximg" class="redactor_input" />' +
         '<label>Author</label>' +
         '<input type="text" id="a2xauthor" class="redactor_input" />' +
         '<label>Url (optional)</label>' +
@@ -61,9 +66,10 @@ RedactorPlugins.a2x = {
       '</div>',
     quote_src: String() +
     '<blockquote>' +
-    '%quote%' +
+    '%image%' +
+    '<p>%quote%</p>' +
     '<footer>' +
-      '<a href="%link%" target="_blank">%author%</a>' +
+      '%author%' +
     '</footer>' +
     '</blockquote>',
     quote_src_without_footer: String() +
